2926b2974a1dc0c7fd9a60a9817d98488e509e0d
NRaykov <nediotenev@gmail.com>
2021-04-22 18:25:46 +0300

    Implement Sidebar

--- src/components/LayoutContainer.vue
+++ src/components/LayoutContainer.vue
@@ -1,23 +1,54 @@
 <template>
   <div class="container-layout">
     <sidebar />
-    <router-view />
+    <transition
+      name="fade"
+      mode="out-in"
+      @beforeLeave="beforeLeave"
+      @enter="enter"
+      @afterEnter="afterEnter"
+    >
+      <router-view />
+    </transition>
   </div>
 </template>
 
 <script>
 import Sidebar from './elements/Sidebar/SidebarContainer.vue';
-// import { mapGetters } from 'vuex';
 
 export default {
   components: {
     Sidebar,
   },
   data() {
-    return {};
+    return {
+      prevHeight: 0,
+    };
   },
   computed: {},
   mounted() {},
-  methods: {},
+  methods: {
+    /*
+    * Implement Transition on route change
+    */
+    beforeLeave(element) {
+      this.prevHeight = getComputedStyle(element).height;
+    },
+    enter(element) {
+      const { height } = getComputedStyle(element);
+
+      // eslint-disable-next-line no-param-reassign
+      element.style.height = this.prevHeight;
+
+      this.$nextTick(() => {
+        // eslint-disable-next-line no-param-reassign
+        element.style.height = height;
+      });
+    },
+    afterEnter(element) {
+      // eslint-disable-next-line no-param-reassign
+      element.style.height = 'auto';
+    },
+  },
 };
 </script>
